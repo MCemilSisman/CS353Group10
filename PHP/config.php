f3ccb218173ed1c68ba1efc87cf01c0db27a5b5a
Tarık Emin Kaplan <tarikekaplan@gmail.com>
2020-01-01 00:29:46 +0300

    Delete config.php

--- PHP/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// db credentials
-$DATABASE_HOST = 'dijkstra.ug.bcc.bilkent.edu.tr';
-$DATABASE_USER = ''; //name.surname
-$DATABASE_PASS = ''; //get from the myunp text file
-$DATABASE_NAME = ''; //name_surname
-?>
